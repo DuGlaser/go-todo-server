2c05e65e0f4899d7d9ddce8c0975434fb7a70dbd
DuGlaser <duglaser0618@gmail.com>
2020-10-31 14:11:54 +0900

    :sparkles: Add the todo dto to the domain layer

---
+++ domain/todo/todo_dto.go
@@ -0,0 +1,25 @@
+package todo_dto
+
+import "errors"
+
+type Todo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+
+func (t *Todo) Validate() error {
+	if t.Title == "" {
+		return errors.New("title is empty")
+	}
+
+	if t.Description == "" {
+		return errors.New("description is empty")
+	}
+
+	if t.Status == "" {
+		return errors.New("status is empty")
+	}
+
+	return nil
+}
