3d10e098f81e8498ea58fe6628885e3d0b6ecbe7
DuGlaser <duglaser0618@gmail.com>
2020-11-01 16:56:59 +0900

    :truck: Move the main to the app directory

---
+++ app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/DuGlaser/go-todo-server/db"
+	"github.com/DuGlaser/go-todo-server/todo/controller/http"
+	"github.com/DuGlaser/go-todo-server/todo/repository/mysql"
+	"github.com/DuGlaser/go-todo-server/todo/usecase"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	todoRepo := mysql.NewMysqlTodoRepository(db.DBConn)
+	todoUsecase := usecase.NewTodoUsecase(todoRepo)
+	http.NewTodoHandler(router, todoUsecase)
+
+	router.Run(":8080")
+}
