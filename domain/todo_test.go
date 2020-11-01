a12c535ccb842bf464230333ed8754809bb2d186
DuGlaser <duglaser0618@gmail.com>
2020-11-01 20:05:52 +0900

    :white_check_mark: Add the todo domain test

---
+++ domain/todo_test.go
@@ -0,0 +1,90 @@
+package domain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidTodo(t *testing.T) {
+	todo := Todo{
+		ID:          1,
+		Title:       "title",
+		Description: "description",
+		Status:      "Doing",
+	}
+
+	err := todo.Validate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, "title", todo.Title)
+	assert.EqualValues(t, "description", todo.Description)
+	assert.EqualValues(t, Doing, todo.Status)
+}
+
+func TestInvalidTitleTodo(t *testing.T) {
+	todo := Todo{
+		ID:          1,
+		Title:       "",
+		Description: "description",
+		Status:      "Doing",
+	}
+
+	err := todo.Validate()
+	assert.EqualValues(t, "title is empty", err.Error())
+}
+
+func TestInvalidDescriptionTodo(t *testing.T) {
+	todo := Todo{
+		ID:          1,
+		Title:       "title",
+		Description: "",
+		Status:      "Doing",
+	}
+
+	err := todo.Validate()
+	assert.EqualValues(t, "description is empty", err.Error())
+}
+
+func TestInvalidStatusTodo(t *testing.T) {
+	todo := Todo{
+		ID:          1,
+		Title:       "title",
+		Description: "description",
+		Status:      "status",
+	}
+
+	err := todo.Validate()
+	assert.EqualValues(t, "invalid status", err.Error())
+}
+
+func TestStatusIsDone(t *testing.T) {
+	todo := Todo{
+		ID:          1,
+		Title:       "title",
+		Description: "description",
+		Status:      "Done",
+	}
+
+	err := todo.Validate()
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, Done, todo.Status)
+}
+
+func TestTrimSpace(t *testing.T) {
+	todo := Todo{
+		ID:          1,
+		Title:       "  title  ",
+		Description: "  description  ",
+		Status:      "Done",
+	}
+
+	_ = todo.Validate()
+
+	assert.EqualValues(t, "title", todo.Title)
+	assert.EqualValues(t, "description", todo.Description)
+}
