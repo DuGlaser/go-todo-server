e89bd83584544200d500c10f6170da42607da219
DuGlaser <duglaser0618@gmail.com>
2020-11-03 12:37:41 +0900

    :sparkles: Add the process to connect test db

--- db/todo_db.go
+++ db/todo_db.go
@@ -7,22 +7,48 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const (
-	username = "username"
-	password = "password"
-	host     = "127.0.0.1:3306"
-	schema   = "db_name"
-)
+type DB struct {
+	Host     string
+	Username string
+	Password string
+	DBName   string
+	Port     string
+}
 
-var DBConn *sql.DB
+func NewDB() *sql.DB {
+	c := NewTodoConfig()
 
-func init() {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		username, password, host, schema,
+	return newDB(&DB{
+		Host:     c.DB.Production.Host,
+		Username: c.DB.Production.Username,
+		Password: c.DB.Production.Password,
+		DBName:   c.DB.Production.DBName,
+		Port:     c.DB.Production.Port,
+	})
+}
+
+func NewTestDB() *sql.DB {
+	c := NewTodoConfig()
+
+	return newDB(&DB{
+		Host:     c.DB.Test.Host,
+		Username: c.DB.Test.Username,
+		Password: c.DB.Test.Password,
+		DBName:   c.DB.Test.DBName,
+		Port:     c.DB.Test.Port,
+	})
+}
+
+func newDB(d *DB) *sql.DB {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
+		d.Username,
+		d.Password,
+		d.Host,
+		d.Port,
+		d.DBName,
 	)
 
-	var err error
-	DBConn, err = sql.Open("mysql", dataSourceName)
+	DBConn, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
@@ -32,4 +58,5 @@ func init() {
 	}
 
 	fmt.Println("Connect mysql!")
+	return DBConn
 }
